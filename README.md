6ef74e01d487004c5de3bdbf08a8f84619f488e4
Tommy <nghthuan98@gmail.com>
2020-06-23 17:22:58 +0700

    Use JSX for project => Reduce code

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Demo: localhost
  ### Video-06- Create React App
     We use React library to build the UI elements. To use React write the command 
     "npx create-react-app frontend" (frontend is name of folder)
+
+### Part 7: Render Products (JSX)
+This is the home page of e-commerce. It shows a list of products.
+Create data.js like class in C# to use OOP.
+
+##### JSX (JavaScript XML)
+JSX is an XML/HTML - like syntax used by React that extends ECMAScript so that XML/HTML -like text can co-exits with JavaScript/React code. The syntax is intended to be used by preprocessors (i.e., transpilers like Babel) to transform HTML-like text found in JavaScript files into standard JavaScript objects that a JavaScript engine will parse.
+Basically, by using JSX you can write concise HTML/XML-like structures (e.g., DOM like tree structures) in the same file as you write JavaScript code, then Babel will transform these expressions into actual JavaScript code. Unlike the past, instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript.
--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import data from './data'
 import './App.css';
 
 class App extends Component {
@@ -40,72 +40,20 @@ class App extends Component {
         <main className="main">
             <div className="content">
                 <ul className="products">
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
-                <li>
-                    <div className="product">
-                        <img className="product-image" src="./images/d1.jpg" alt="product"></img>
-                        <div className="product-name">
-                            <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Starts (10 Reviews)</div>
-                    </div>
-                </li>
+                    {
+                        data.products.map(product =>
+                            <li>
+                            <div className="product">
+                                <img className="product-image" src={product.image} alt="product"></img>
+                                <div className="product-name">
+                                    <a href="product.html">{product.name}</a>
+                                </div>
+                                <div className="product-brand">{product.brand}</div>
+                                <div className="product-price">{product.price}</div>
+                                <div className="product-rating">{product.rating} Starts {product.numReviews}</div>
+                            </div>
+                        </li>)
+                    }
                 </ul>
             </div>
         </main>
